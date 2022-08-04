396fcf93927d6f62d2231722f7991a0076d2ecb9
Haider Ali <haidr.ali@aldaimsolutions.com>
2022-08-05 01:01:41 +0500

    FrontEnd & Backend Server Done

---
+++ client/src/screens/signUp.js
@@ -0,0 +1,148 @@
+import * as React from 'react';
+import { useState } from 'react';
+import {Button,CssBaseline,Container,Box,Typography,Grid,TextField,Link,Checkbox,FormControlLabel} from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import signup_logo from '../assets/signup_logo.png'
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        myWorld.com
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+const theme = createTheme();
+
+export default function SignUP() {
+  
+  const [firstName,setfirstName]=useState('');
+  const [lastName,setlastName]=useState('');
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  const [agree,setAgree]=useState(false);
+  
+ async function handleSubmit(event){
+     event.preventDefault();
+     const data=new FormData(event.currentTarget);
+    let allEntered=data.get('firstName')!==''&&data.get('lastName')!==''&&data.get('email')!==''&&data.get('password')!==''&&agree
+    if(allEntered){
+    const response= await fetch('http://localhost:1337/api/register',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+        })
+      })
+    const data=await response.json()
+    console.log('=============Server Response============');
+    console.log(data);
+    console.log('========================================');  
+    }
+    else { alert("Please Fill all the fields") }
+
+  };
+  
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <img src={signup_logo}  className='signup-logo' alt="signUP"/>
+          <Typography component="h1" variant="h5" className='signup-header'>
+            Sign up in to my World
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  onChange={(e) => setfirstName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  onChange={(e) => setlastName(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+               <Checkbox sx={{ml:-1.5}} onClick={()=>setAgree(!agree)} value="allowExtraEmails" color="primary" />
+                I agree to all <Link href="#" style={{textDecoration:'none'}}>terms & conditions</Link>
+              </Grid>
+
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Link href="#" variant="body2" align='center'>
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
-//E -Express
+//E -Express on Nodejs
 
 const express = require('express');
-const app = express();
+const cors =require('cors');
+const app = express()
 
-app.get('/hello', (req, res) => {
-    res.send("hello server")
+app.use(cors())//cors is a middleware (get response and send it other port)
+app.use(express.json())
+
+app.post('/api/register',(req,res)=>{
+    console.log(req.body)
+    res.json({status:'User has been Registered'})
 })
 
 app.listen(1337, () => {
-    console.log("Server Running on 1337")
+    console.log("Server is Running at:");
+    console.log("http://localhost:1337/")
 })
